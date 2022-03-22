402f91370740459db5c73ad7251276f36ab8c59e
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-22 15:03:42 +0100

    created and styled no-notes div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Split from "react-split";
+import { nanoid } from "nanoid";
 
 export default function App() {
+  const [notes, setNotes] = React.useState([]);
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your note's title here",
+    };
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+    setCurrentNoteId(newNote.id);
+  }
+
   return (
-    <div className="container">
-      <h1>App works!</h1>
-    </div>
+    <main>
+      <div className="no-notes">
+        <h1>You have no notes</h1>
+        <button className="first-note" onClick={createNewNote}>
+          Create one now
+        </button>
+      </div>
+    </main>
   );
 }
