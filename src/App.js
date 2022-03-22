163a05b1bdae1753bae5af4b2dab02de643ce3bb
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-22 16:14:23 +0100

    added and styled Split bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Split from "react-split";
 import { nanoid } from "nanoid";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
 
 export default function App() {
   const [notes, setNotes] = React.useState([]);
@@ -19,12 +21,24 @@ export default function App() {
 
   return (
     <main>
-      <div className="no-notes">
-        <h1>You have no notes</h1>
-        <button className="first-note" onClick={createNewNote}>
-          Create one now
-        </button>
-      </div>
+      {notes.length > 0 ? (
+        <Split
+          sizes={[30, 70]}
+          direction="horizontal"
+          gutterSize={15}
+          className="split"
+        >
+          <Sidebar />
+          <Editor />
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
     </main>
   );
 }
